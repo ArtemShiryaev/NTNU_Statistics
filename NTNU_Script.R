ddd1ba49fea2ff70d1ab50305e4625445a24b3de
ArtemShiryaev <47121889+ArtemShiryaev@users.noreply.github.com>
2023-11-27 21:18:05 +0100

    Updates

--- NTNU_Script.R
+++ NTNU_Script.R
@@ -1,9 +1,15 @@
+############################
+# Artem Angelchev Shiryaev #
+#      NTNU Case Script    #
+############################
+
+# Libraries
 library(pwr)
 library(pwrss)
 library(ggplot2)
 library(scales)
 
-
+# Proportions
 Prop_DrugA <- 0.5 
 Prop_DrugB <- 0.7
 
@@ -37,6 +43,7 @@ diff_size <- pwr.2p2n.test(h=round(h_prop, digits = 2),
                            sig.level = Alpha,
                            alternative="two.sided")
 
+# Chi2 power calculations
 chi2_tst <- pwr.chisq.test(w = (-1)*round(h_prop, digits = 2),
                            N = NULL,
                            df = (91-1)*(2-1),#(r-1)(c-1),
@@ -56,7 +63,25 @@ np_prop <- pwrss.np.2groups(mu1 = round(h_prop, digits = 2),
                             distribution = "normal"
 )
 
+paired_t <- pwrss.t.2means(mu1 = round(h_prop, digits = 2),
+                           alternative = "not equal",
+                           paired = TRUE,
+                           paired.r = 0.2,
+                           alpha = Alpha,
+                           power = Beta
+                           
+)
 
+
+paired_np_2groups <- pwrss.np.2groups(mu1 = round(h_prop, digits = 2),
+                                      mu2 = 0,
+                                      paired = TRUE,
+                                      paired.r = 0.5,
+                                      alternative = "not equal",
+                                      power = Beta,
+                                      alpha = Alpha,
+                                      distribution = "normal"
+)
 
 
 
@@ -66,3 +91,5 @@ plot(z_prop)
 plot.power.htest(two_test, main = "Two proportions (same sample sizes)")
 plot.power.htest(diff_size, main = "Two proportions (different sample sizes)")
 plot.power.htest(chi2_tst, main = "Chi^2 Power Calculation")
+paired_np_2groups
+paired_t
