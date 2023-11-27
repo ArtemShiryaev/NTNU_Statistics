40350bccd4184f94de29f5c38d38bd768c70c781
ArtemShiryaev <47121889+ArtemShiryaev@users.noreply.github.com>
2023-11-27 19:13:13 +0100

    Commit initial
    Commit initial

---
+++ NTNU_Script.R
@@ -0,0 +1,68 @@
+library(pwr)
+library(pwrss)
+library(ggplot2)
+library(scales)
+
+
+Prop_DrugA <- 0.5 
+Prop_DrugB <- 0.7
+
+Alpha <- 0.05 # Significance Level
+Beta <- 0.8   # Power Level
+
+# Difference between Two Proportions (z Test)
+z_prop <- pwrss.z.2props(p1 = Prop_DrugA,
+                         p2 = Prop_DrugB,
+                         alternative = "not equal",
+                         power = Beta,
+                         alpha = Alpha
+                         
+)
+
+
+# Effect Size using Binomial Transform
+h_prop <- 2*asin(sqrt(Prop_DrugA)) - 2*asin(sqrt(Prop_DrugB))
+
+# Power calculation for two proportions (same sample sizes)
+two_test <-pwr.2p.test(h=round(h_prop, digits = 2), 
+                       sig.level = Alpha,
+                       power = Beta,
+                       alternative = "two.sided")
+
+
+# Power Calculation for two proportions (different sizes)
+diff_size <- pwr.2p2n.test(h=round(h_prop, digits = 2),
+                           n1 = 180,
+                           power =  Beta,
+                           sig.level = Alpha,
+                           alternative="two.sided")
+
+chi2_tst <- pwr.chisq.test(w = (-1)*round(h_prop, digits = 2),
+                           N = NULL,
+                           df = (91-1)*(2-1),#(r-1)(c-1),
+                           sig.level = 0.05,
+                           power = 0.8
+                           
+)
+
+
+
+# Difference between Two Groups (Non-parametric Tests for Independent and Paired Samples)
+np_prop <- pwrss.np.2groups(mu1 = round(h_prop, digits = 2),
+                            mu2 = 0,
+                            alternative = "not equal",
+                            power = Beta,
+                            alpha = Alpha,
+                            distribution = "normal"
+)
+
+
+
+
+
+
+# Graphical Results
+plot(z_prop)
+plot.power.htest(two_test, main = "Two proportions (same sample sizes)")
+plot.power.htest(diff_size, main = "Two proportions (different sample sizes)")
+plot.power.htest(chi2_tst, main = "Chi^2 Power Calculation")
