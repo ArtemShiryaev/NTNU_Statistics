75fc96ca1a6922da5cafc4aaa22056f50fbc5cba
ArtemShiryaev <47121889+ArtemShiryaev@users.noreply.github.com>
2023-11-28 13:26:10 +0100

    Commit final push

--- NTNU_Script.R
+++ NTNU_Script.R
@@ -19,7 +19,7 @@ Beta <- 0.8   # Power Level
 # Difference between Two Proportions (z Test)
 z_prop <- pwrss.z.2props(p1 = Prop_DrugA,
                          p2 = Prop_DrugB,
-                         alternative = "not equal",
+                         alternative = "greater",
                          power = Beta,
                          alpha = Alpha
                          
@@ -30,23 +30,22 @@ z_prop <- pwrss.z.2props(p1 = Prop_DrugA,
 h_prop <- 2*asin(sqrt(Prop_DrugA)) - 2*asin(sqrt(Prop_DrugB))
 
 # Power calculation for two proportions (same sample sizes)
-two_test <-pwr.2p.test(h=round(h_prop, digits = 2), 
+two_test <-pwr.2p.test(h=(-1)*round(h_prop, digits = 2), 
                        sig.level = Alpha,
                        power = Beta,
-                       alternative = "two.sided")
+                       alternative = "greater")
 
 
 # Power Calculation for two proportions (different sizes)
-diff_size <- pwr.2p2n.test(h=round(h_prop, digits = 2),
-                           n1 = 180,
+diff_size <- pwr.2p2n.test(h=(-1)*round(h_prop, digits = 2),
+                           n1 = 140,
                            power =  Beta,
                            sig.level = Alpha,
-                           alternative="two.sided")
+                           alternative="greater")
 
-# Chi2 power calculations
 chi2_tst <- pwr.chisq.test(w = (-1)*round(h_prop, digits = 2),
                            N = NULL,
-                           df = (91-1)*(2-1),#(r-1)(c-1),
+                           df = (72-1)*(2-1),#(r-1)(c-1),
                            sig.level = 0.05,
                            power = 0.8
                            
@@ -54,37 +53,54 @@ chi2_tst <- pwr.chisq.test(w = (-1)*round(h_prop, digits = 2),
 
 
 
+# Power calculations for t-tests of means (one sample, two samples and paired samples)
+t_test_2means <- pwr.t.test(d = (-1)*round(h_prop, digits = 2),
+                            sig.level = Alpha,
+                            power = Beta,
+                            type = "two.sample",
+                            alternative = "greater") 
+
+
+
+# Power calculations for two samples (different sizes) t-tests of means
+t.test_diff_means <- pwr.t2n.test(n1 = 140,
+                                  d = (-1)*round(h_prop, digits = 2),
+                                  sig.level = Alpha,
+                                  power = Beta,
+                                  alternative = "greater")
+
+
 # Difference between Two Groups (Non-parametric Tests for Independent and Paired Samples)
-np_prop <- pwrss.np.2groups(mu1 = round(h_prop, digits = 2),
-                            mu2 = 0,
-                            alternative = "not equal",
+np_prop <- pwrss.np.2groups(mu1 = 0,
+                            mu2 = round(h_prop, digits = 2),
+                            alternative = "greater",
                             power = Beta,
                             alpha = Alpha,
                             distribution = "normal"
 )
 
-paired_t <- pwrss.t.2means(mu1 = round(h_prop, digits = 2),
-                           alternative = "not equal",
+# Paired samples
+
+paired_t <- pwrss.t.2means(mu1 = 0,
+                           mu2 = round(h_prop, digits = 2),
+                           alternative = "greater",
                            paired = TRUE,
-                           paired.r = 0.2,
+                           paired.r = 0.5,
                            alpha = Alpha,
                            power = Beta
                            
 )
 
 
-paired_np_2groups <- pwrss.np.2groups(mu1 = round(h_prop, digits = 2),
-                                      mu2 = 0,
+paired_np_2groups <- pwrss.np.2groups(mu2 = round(h_prop, digits = 2),
+                                      mu1 = 0,
                                       paired = TRUE,
                                       paired.r = 0.5,
-                                      alternative = "not equal",
+                                      alternative = "greater",
                                       power = Beta,
                                       alpha = Alpha,
                                       distribution = "normal"
 )
-
-
-
 
 # Graphical Results
 plot(z_prop)
